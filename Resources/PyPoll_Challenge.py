86d013e7721d2e8b536e94d4416946c57105a7d5
Moya112 <105765150+Moya112@users.noreply.github.com>
2022-06-22 17:52:52 -0400

    Add Python Challenge.

--- Resources/PyPoll_Challenge.py
+++ Resources/PyPoll_Challenge.py
@@ -4,7 +4,7 @@
 # Add our dependencies.
 import csv
 import os
-from pip._internal.models import candidate
+
 
 # Assign a variable to load a file from a path.
 file_to_load = os.path.join("Resources", "election_results.csv")
